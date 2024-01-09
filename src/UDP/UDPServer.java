f0c0cf3390bebe774c9804d7ee1c72237e9c507b
Joseba Andoni Herranz <59825238+Joseba-Herranz@users.noreply.github.com>
2024-01-09 10:55:31 +0100

    Update UDPServer.java

--- src/UDP/UDPServer.java
+++ src/UDP/UDPServer.java
@@ -5,7 +5,6 @@ import java.net.DatagramSocket;
 
 public class UDPServer {
     public static void main(String args[]) throws Exception {
-        // Crear un socket UDP en el puerto 9876
         DatagramSocket serverSocket = new DatagramSocket(9876);
 
         byte[] receiveData = new byte[1024];
@@ -13,14 +12,13 @@ public class UDPServer {
         System.out.println("Servidor UDP esperando mensajes...");
 
         while (true) {
-            // Recibir paquete del cliente
+
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
 
             System.out.println("Mensaje recibido: " + message);
 
-            // Si el mensaje es "FIN", termina el bucle
             if (message.equals("FIN")) {
                 break;
             }
