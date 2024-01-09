41258f16a3994020ac861d23c34f77dd33a58b79
Joseba Andoni Herranz <59825238+Joseba-Herranz@users.noreply.github.com>
2024-01-09 10:55:14 +0100

    Update UDPClient.java

--- src/UDP/UDPClient.java
+++ src/UDP/UDPClient.java
@@ -6,14 +6,13 @@ import java.net.InetAddress;
 
 public class UDPClient {
     public static void main(String args[]) throws Exception {
-        // Crear un socket UDP
+        
         DatagramSocket clientSocket = new DatagramSocket();
 
-        InetAddress IPAddress = InetAddress.getByName("localhost"); // Cambiar a la dirección IP del servidor si es necesario
+        InetAddress IPAddress = InetAddress.getByName("localhost"); 
 
         byte[] sendData;
 
-        // Enviar 10.000 mensajes
         for (int i = 0; i < 10000; i++) {
             String sentence = "Mensaje número " + i;
             sendData = sentence.getBytes();
@@ -21,7 +20,7 @@ public class UDPClient {
             clientSocket.send(sendPacket);
         }
 
-        // Enviar mensaje de finalización
+
         String finalMessage = "FIN";
         sendData = finalMessage.getBytes();
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
